691f56c8ee2ae6225c183c3c3b44a876140aa053
wuicho123 <115720745+wuicho123@users.noreply.github.com>
2022-10-14 08:50:56 -0500

    Add files via upload

---
+++ LUISJAVIER/index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+
+<?php
+echo "<h2>PHP is FUN¡</h2>";
+echo "Hello World!<br>";
+echo "I´m about to learn PHP!<br>";
+echo "This","string","was", "made", "with multiple parameters.";
+?>
+
+<?php
+print "<h2>PHP is FUN¡</h2>";
+print "Hello World!<br>";
+print "I´m about to learn PHP!<br>";
+
+
+?>
+
+
+</head>
+<body>
+     
+</body>
+</html>
